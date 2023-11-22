796730736be7bad0ee68250de5e8a3a80294b076
Guilherme Benicio <guilhermebenicio18102003@gmail.com>
2023-11-22 00:32:41 -0300

    like

--- src/Components/Photo/PhotoContentCurtida.jsx
+++ src/Components/Photo/PhotoContentCurtida.jsx
@@ -7,16 +7,15 @@ import coracaovazio from '../../assets/coracaovazio.svg';
 import { userContext } from '../../userContext';
 
 const PhotoContentCurtida = ({ curtidas, usuario, _id }) => {
-  const [like, setLike] = React.useState(curtidas.length);
-  const [isLike, setIsLike] = React.useState(() => {
-    // Verifica se o like está salvo no localStorage
-    const savedLike = window.localStorage.getItem(`like_${_id}`);
-    return savedLike ? JSON.parse(savedLike) : '';
-  });
+  const [curtidasArray, setCurtidasArray] = React.useState(curtidas);
   const { dataUser } = React.useContext(userContext);
+  const [like, setLike] = React.useState(curtidas.length);
+  const [islike, setIsLike] = React.useState(() =>
+    curtidasArray.includes(dataUser.usuario),
+  );
+
   const { request } = useFetch();
   const token = window.localStorage.getItem('token');
-
   async function handleClick() {
     const { url, options } = POST_LIKE(
       {
@@ -26,27 +25,32 @@ const PhotoContentCurtida = ({ curtidas, usuario, _id }) => {
       token,
     );
     const { response, json } = await request(url, options);
+
+    if (islike) {
+      const novasCurtidas = curtidasArray.filter(
+        (nome) => nome !== dataUser.usuario,
+      );
+      setCurtidasArray(novasCurtidas);
+    } else {
+      const novasCurtidas = [...curtidasArray, usuario];
+      setCurtidasArray(novasCurtidas);
+    }
+    setIsLike(!islike);
+
     if (response.ok) {
       if (json.tipo === 'like') {
         setLike(like + 1);
-        setIsLike(true);
       } else if (json.tipo === 'dislike') {
         setLike(like - 1);
-        setIsLike(false);
       }
     }
   }
-
-  // Atualiza o localStorage quando o estado do like muda
-  React.useEffect(() => {
-    window.localStorage.setItem(`like_${_id}`, JSON.stringify(isLike));
-  }, [isLike, _id]);
 
   return (
     <>
       {dataUser ? (
         <span className={styles.curtidas} onClick={handleClick}>
-          {isLike ? (
+          {islike ? (
             <img
               src={coracaopreenchido}
               alt="Likes"
